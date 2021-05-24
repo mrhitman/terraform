5e2fd3c39b55ffad14f11c66fe685e4784076517
One <kabalx47@gmail.com>
2021-05-24 19:40:48 +0300

    add workspaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const { resolve, join, basename } = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
-const { readdirSync, lstatSync } = require('fs');
+const { readdirSync, lstatSync, mkdirSync, existsSync } = require('fs');
 
 const isDirectory = source => lstatSync(source).isDirectory()
 const getDirectories = source =>
     readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+const path = resolve(__dirname, 'dist', 'lambdas');
+if (!existsSync(path)) {
+    mkdirSync(path, { recursive: true });
+}
 
 const directories = getDirectories(resolve(__dirname, 'src', 'lambdas'))
 const config = {
     mode: 'production',
     target: 'node',
     entry: directories.reduce((acc, lambdaPath) => ({ ...acc, [basename(lambdaPath)]: lambdaPath }), {}),
+    optimization: {
+        minimize: true
+    },
     module: {
         rules: [
             {
@@ -27,7 +35,7 @@ const config = {
                 onEnd: {
                     archive:
                         getDirectories(
-                            resolve(__dirname, 'dist', 'lambdas')).map(lambdaDistPath => {
+                            resolve(path)).map(lambdaDistPath => {
                                 return {
                                     source: lambdaDistPath,
                                     destination: resolve(lambdaDistPath, '..', basename(lambdaDistPath) + '.zip')
